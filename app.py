fc4ef2e887163e3a4c3cd7d021a259dfa97d61d0
Abood93rizik <abdullahsmadi551@gmail.com>
2025-04-27 00:31:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-last_signal = None
+# خزّن آخر إشارة في متغير جلوبال
+last_signal_data = {"last_signal": None}
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"last_signal": last_signal})
+    return jsonify(last_signal_data)
 
 @app.route("/update_signal", methods=["POST"])
 def update_signal():
-    global last_signal
     data = request.get_json()
     if data and "signal" in data:
-        last_signal = data["signal"]
+        last_signal_data["last_signal"] = data["signal"]
         return jsonify({"message": "Signal updated successfully"}), 200
     else:
         return jsonify({"error": "Invalid data"}), 400
